bf12773086e189f21cfecf9964034af22e0562ca
Deny Nakano <deny.nakano@gmail.com>
2022-09-27 21:46:31 -0300

    Error route done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Home } from './Pages/Home';
 import { NewRecipe } from './Pages/NewRecipe';
 import { Recipe } from './Pages/Recipe';
 import { Search } from './Pages/Search';
+import { Error } from './Pages/Error';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
         <Route path='/search' element= {<Search/>} />
         <Route path='/recipe/:id'element= {<Recipe/>} />
         <Route path='/new-recipe' element= {<NewRecipe/>} />
-        
+        <Route path='*' element= {<Error/>} />
       </Routes>
 
   
