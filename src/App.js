476dd06b124a7b2d2fd85a466c20e1b7e2962b28
Deny Nakano <deny.nakano@gmail.com>
2022-09-27 21:41:08 -0300

    Recipe route done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import { About } from './Pages/About';
 import { Home } from './Pages/Home';
 import { NewRecipe } from './Pages/NewRecipe';
+import { Recipe } from './Pages/Recipe';
 import { Search } from './Pages/Search';
 
 function App() {
@@ -14,7 +15,9 @@ function App() {
         <Route path='/' element= {<Home/>} /> 
         <Route path='/about' element= {<About/>} /> 
         <Route path='/search' element= {<Search/>} />
+        <Route path='/recipe/:id'element= {<Recipe/>} />
         <Route path='/new-recipe' element= {<NewRecipe/>} />
+        
       </Routes>
 
   
