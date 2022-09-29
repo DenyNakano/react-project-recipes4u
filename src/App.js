279ade80dc887f1058f818507ddb8333082d7f4f
Shania <shaniaantezana@gmail.com>
2022-09-28 20:10:35 -0400

    added components CardCarrousel.js, Carousel.js and NavBar.js with your styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 
 import { Routes, Route  } from 'react-router';
-
-import './App.css';
 import { About } from './Pages/About';
 import { Home } from './Pages/Home';
 import { NewRecipe } from './Pages/NewRecipe';
@@ -9,6 +7,7 @@ import { Recipe } from './Pages/Recipe';
 import { Search } from './Pages/Search';
 import { Error } from './Pages/Error';
 import { NavBar } from './Components/NavBar';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
@@ -22,9 +21,6 @@ function App() {
         <Route path='/new-recipe' element= {<NewRecipe/>} />
         <Route path='*' element= {<Error/>} />
       </Routes>
-
-  
-      
     </div>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button} from 'reactstrap'
-
-export const NavBar = (props) => {
-   return( <div>
-      <Button
-    color="primary"
-  >
-    Click Me
-  </Button>
+import React from "react";
+import { NavItem, Nav, NavLink } from "reactstrap";
+import "./NavBar.css";
+export const NavBar = () => {
+  return (
+    <div className="main" >
+      <Nav className="nav" >
+        <NavItem>
+          <NavLink href="/">
+              Image
+          </NavLink>
+        </NavItem>
+        <div className="nav-links" >
+        <NavItem>
+          <NavLink href="/search">
+            Search
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/about" >
+            About
+          </NavLink>
+        </NavItem>
+        </div>
+      </Nav>
     </div>
-   )
-}
+  );
+};
