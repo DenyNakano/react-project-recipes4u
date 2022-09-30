6f640f6f351d319640609c3a8d95dcebbd3fa5e6
Shania <shaniaantezana@gmail.com>
2022-09-30 13:20:13 -0400

    create folder images and file imageLogo.jpeg

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,13 +2,14 @@
 import React from "react";
 import { NavItem, Nav, NavLink } from "reactstrap";
 import "./NavBar.css";
+import recipeLogo from '../images/recipeLogo.jpeg'
 export const NavBar = () => {
   return (
     <div className="main" >
       <Nav className="nav" >
         <NavItem>
           <NavLink href="/">
-              Image
+            <img id="recipe-logo" src={recipeLogo} alt={recipeLogo}/>
           </NavLink>
         </NavItem>
         <div className="nav-links" >
