20257a0e2ffc83f03c169ce6ac5399dd797cd28c
Deny Nakano <deny.nakano@gmail.com>
2022-10-01 17:29:56 -0300

    NewRecipe done

--- src/Pages/NewRecipe.js
+++ src/Pages/NewRecipe.js
@@ -1,7 +1,111 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export const NewRecipe = () => {
+  const [Autor, setAutor] = useState('')
+  const [Receita, setReceita] = useState('')
+  const [Descrição, setDescrição] = useState('')
+  const [Ingredientes, setIngredientes] = useState('')
+  const [Preparo, setPreparo] = useState('')
+  const [Imagem, setImagem] = useState('')
+  const [Categoria, setCategoria] = useState('')
+  const [Tempo, setTempo] = useState('')
+  const [Dificuldade, setDificuldade] = useState('') 
+  const [Avaliação, setAvaliação] = useState('')                                                                                    
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault () 
+    let NewRecipe = {
+      type: Categoria,
+      name: Receita,
+      description: Descrição,
+      ingredients: Ingredientes.split(','),
+      preparation: Preparo.split(','),
+      time: Tempo,
+      level: Dificuldade,
+      rate: Avaliação,
+      image: Imagem,
+    }
+    
+    try {
+      await axios.post(
+        "https://ironrest.herokuapp.com/recipes4u" , NewRecipe
+      );
+      setCategoria('')
+      setReceita('')
+      setDescrição('')
+      setIngredientes('')
+      setPreparo('')
+      setTempo('')
+      setDificuldade('')
+      setAvaliação('')
+      setImagem('')
+      navigate('/')
+
+    } catch (error) {
+    console.log(error);
+  }}  
   return (
-    <div>New Recipe</div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Autor: </label>
+        <input type="text" value={Autor} onChange={(e) =>{setAutor(e.target.value)}}/>
+      </div>
+      <div>
+        <label>Receita: </label>
+        <input type="text" value={Receita} onChange={(e) =>{setReceita(e.target.value)}}/>
+      </div>
+      <div>
+        <label>Descrição: </label>
+        <input type="text" value={Descrição} onChange={(e) =>{setDescrição(e.target.value)}}/>
+      </div>
+      <div>
+        <label>Ingredientes: </label>
+        <input type="text" value={Ingredientes} onChange={(e) =>{setIngredientes(e.target.value)}}/>
+      </div>
+      <div>
+        <label>Preparo: </label>
+        <input type="text" value={Preparo} onChange={(e) =>{setPreparo(e.target.value)}}/>
+      </div>
+      <div>
+        <label>Imagem: </label>
+        <input type="text" value={Imagem} onChange={(e) =>{setImagem(e.target.value)}}/>
+      </div>
+      <div>
+        <label>Categoria: </label>
+        {/*<input type="text" value={Categoria} onChange={(e) =>{setCategoria(e.target.value)}}/>*/}
+        <select value={Categoria} onChange={(e) =>{setCategoria(e.target.value)}}>
+          <option>Prato Principal</option>
+          <option>Drinks</option>
+          <option>Acompanhamentos</option>
+          <option>Massas</option>
+          <option>Sobremesas</option>
+        </select>
+      </div>
+      <div>
+        <label>Tempo: </label>
+        <input type="text" value={Tempo} onChange={(e) =>{setTempo(e.target.value)}}/>
+      </div>
+      <div>
+        <label>Dificuldade: </label>
+        {/*<input type="text" value={Dificuldade} onChange={(e) =>{setDificuldade(e.target.value)}}/>*/}
+        <select value={Dificuldade} onChange={(e) =>{setDificuldade(e.target.value)}}>
+          <option>😄 Fácil</option>
+          <option>😅 Moderado</option>
+          <option>😓 Difícil</option>
+        </select>
+      </div>
+      <div>
+        <label>Avaliação: </label>
+        <input type="text" value={Avaliação} onChange={(e) =>{setAvaliação(e.target.value)}}/>
+      </div>
+      <button>
+        Salvar
+      </button>
+    </form>
+
   )
-}
+  }
+
--- src/Pages/RecipeDetails.js
+++ src/Pages/RecipeDetails.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState }from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
 
 export const RecipeDetails = () => {
-  const {id} = useParams()
+  const { id } = useParams()
   const [recipe, setRecipe] = useState({});
 
   useEffect(() => {
@@ -12,10 +12,10 @@ export const RecipeDetails = () => {
   }, []);
   const getRecipe = async () => {
     try {
-      const {data} = await axios.get(
+      const { data } = await axios.get(
         `https://ironrest.herokuapp.com/recipes4u/${id}`
       );
-      
+
       setRecipe(data);
     } catch (error) {
       console.log(error);
@@ -24,7 +24,7 @@ export const RecipeDetails = () => {
   return (
     <div>
       <div>
-        <img src={recipe?.image} alt={recipe?.image}/>
+        <img src={recipe?.image} alt={recipe?.image} />
         <h1>{recipe.name}</h1>
         <h2>Descrição</h2>
         <p>
@@ -32,24 +32,24 @@ export const RecipeDetails = () => {
         </p>
         <h2>Ingredientes</h2>
         <ul>
-        {recipe?.ingredients?.map((ingredient,index)=>
-         <li key={index}>{ingredient}</li> 
-        )}
+          {recipe?.ingredients?.map((ingredient, index) =>
+            <li key={index}>{ingredient}</li>
+          )}
         </ul>
         <div>
           {recipe?.time} {recipe?.level} {recipe?.rate}
         </div>
         <h2>Preparo</h2>
         <ul>
-          {recipe?.preparation?.map((step,index) => 
-          <li key={index}>
-            {index + 1} {step}
-          </li>
-        )}
-          </ul>
-          
+          {recipe?.preparation?.map((step, index) =>
+            <li key={index}>
+              {index + 1} {step}
+            </li>
+          )}
+        </ul>
+
       </div>
-      
+
     </div>
   )
 }
