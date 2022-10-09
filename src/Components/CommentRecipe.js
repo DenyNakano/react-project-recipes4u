c0cc056bc5e495c908c96836bfdcc365d3dab6bd
Shania <shaniaantezana@gmail.com>
2022-10-08 22:48:51 -0400

    add Rate in CommentUser

--- src/Components/CommentRecipe.js
+++ src/Components/CommentRecipe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { MdStar } from "react-icons/md";
 export const CommentRecipe = ({filteredComments}) => {
 const timeFormat = (time) =>{
     return `${time.getDate()}/${time.getMonth() + 1}/${time.getFullYear()}`
@@ -13,6 +13,22 @@ const timeFormat = (time) =>{
        <div key={filteredComment._id} >
         <p>Autor :{filteredComment.author}</p>
         <p>comment: {filteredComment.comment}</p>
+        <p>
+        {[...Array(Number(filteredComment.rate))].map((star) => 
+                    
+                    <MdStar
+                
+                      color="#ffc107"
+                      size={50}
+                />)}
+                  {[...Array(5-Number(filteredComment.rate))].map((star) => 
+                    <MdStar
+                      
+                      color="#e4e5e9"
+                      size={50}
+
+                  />)}
+        </p>
         <p>data:{timeFormat(new Date(filteredComment.created_at))}</p>
         <p>horário:{timeHour(new Date(filteredComment.created_at))}</p>
        
