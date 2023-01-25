2410ee32ced98de67b2da2d67a1679dcd4fd3f08
Shania <shaniaantezana@gmail.com>
2023-01-25 19:38:21 -0400

    create file api.utils.js

---
+++ src/utils/api.utils.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+class Api {
+    constructor() {
+        this.api = axios.create({
+            baseURL: 'http://localhost:5055/'
+        })
+    }
+    bestRecipes = async () => {
+        try {
+            const { data } = await this.api.get('/recipe/best')
+            return data
+        } catch (error) {
+            throw error
+        }
+    }
+}
+export default new Api()
